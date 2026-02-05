f30a61459394c2fd647ed909fb22f55cd224933a
KrVicky2002 <vk4722824@gmail.com>
2026-02-05 22:49:30 +0530

    this is a part of python problem solving series and this is problem one that i solved.

---
+++ problem1.py
@@ -0,0 +1,17 @@
+# Pyhton program that takes a list of numbers , removes all duplicate elements and returns new list. 
+#steps
+# create a new empty list
+# go through each element of the list one by one by one by using loop (for).
+# add an element to the new list only if already not present in it.
+
+numbers = [1,2,3,2,4,4,5,6,3]
+
+newList = []
+
+
+for ele in numbers:
+    if(ele not in newList):
+        newList.append(ele)
+
+
+print(newList)
